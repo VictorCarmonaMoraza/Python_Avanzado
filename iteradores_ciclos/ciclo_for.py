3117ebe8b439229c155236c4fb42ab5ac17b51c0
Victor Carmona <victorcarmona9035.com>
2025-05-19 20:27:16 +0200

    Sentencia break y continue en el ciclo for

--- iteradores_ciclos/ciclo_for.py
+++ iteradores_ciclos/ciclo_for.py
@@ -2,3 +2,27 @@
 nombres_list = ['Juan', 'Pedro', 'Maria', 'Ana']
 for nombre in nombres_list:
     print(nombre)
+
+
+print(f'----------------')
+#Imprime Juan y Pedro
+for nombre in nombres_list:
+    if nombre == 'Maria':
+        #Se para y no itera sobre los siguientes elementos
+        break
+    print(nombre) 
+
+print(f'----------------')
+#No imprirme Maria la pasa de largo
+for nombre in nombres_list:
+    if nombre == 'Maria':
+        #Se imprime todo lo que sea diferente a Maria
+        continue
+    print(nombre) 
+
+
+print(f'----------------')
+#No imprirme nada ya que nos estamos saltado todos los nombres
+for nombre in nombres_list:
+    continue
+    print(nombre)
