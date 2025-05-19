ff18e93067c3c8ce8ab35a248a6b1cfd36c4fa15
Victor Carmona <victorcarmona9035.com>
2025-05-19 08:24:05 +0200

    Parametros y argumentos de una funcion Pyhton

---
+++ parametros_argumentos.py
@@ -0,0 +1,8 @@
+
+def caclular_area_cuadrado(Lado):
+    """Calcular el area de un cuadrado """
+    area = Lado * Lado
+    return area
+
+areacuadrado = caclular_area_cuadrado(Lado=5)
+print(f"El area del cuadrado es: {areacuadrado}")
